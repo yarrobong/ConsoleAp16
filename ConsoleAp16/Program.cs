d6d1fadaf2e25c0d45a8e9b61c1ae0ab1027063e
yarrobong <oneboardsholi@gmail.com>
2023-10-28 02:47:03 +0500

    Добавьте файлы проекта.

---
+++ ConsoleAp16/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Введите строку для проверки:");
+string str = Console.ReadLine();
+
+bool isValidHexadecimalNumber = true;
+
+if (string.IsNullOrWhiteSpace(str))
+{
+    isValidHexadecimalNumber = false;
+}
+else
+{
+    // Удалите возможные префиксы "0x" или "0X"
+    if (str.StartsWith("0x") || str.StartsWith("0X"))
+    {
+        str = str.Substring(2);
+    }
+
+    foreach (char digit in str)
+    {
+        if (!IsHexadecimalDigit(digit))
+        {
+            isValidHexadecimalNumber = false;
+            break;
+        }
+    }
+}
+
+if (isValidHexadecimalNumber)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
+    }
+
+    // Функция для проверки, является ли символ шестнадцатеричной цифрой (0-9, A-F, a-f)
+    static bool IsHexadecimalDigit(char digit)
+{
+    return (digit >= '0' && digit <= '9') ||
+           (digit >= 'A' && digit <= 'F') ||
+           (digit >= 'a' && digit <= 'f');
